d99c63cbc8e9be954f40fde039e7551fc21a66ef
Rift343 <matthieu.conrath@gmail.com>
2025-02-06 08:43:12 +0100

    end

--- src/Controller/FactController.php
+++ src/Controller/FactController.php
@@ -17,9 +17,35 @@ final class FactController extends AbstractController
 
     #[Route(path:'/mastermind', name:'app_mastermind')]
     public function app_mastermind(Request $request): Response{
-
-        return $this->render(view:'/fact/mastermind.html.twig',parameters: []);
+        $session = $request->getSession();
+        #$id = uniqid("ABC",true);
+        $mastermind =new MastermindService(4);
+        $session->set("jeu",$mastermind);
+        return $this->render(view:'/fact/mastermind.html.twig',parameters: ["jeu"=>$mastermind]);
     }
+
+    #[Route(path:"/mastermind/sendTry", name:"sendTry")]
+    public function sendTry(Request $request): Response{
+        $session = $request->getSession();
+        $mastermind = $session->get("jeu");
+       # if ($mastermind == -1) {
+        #    return $this->render(view: "/base.html.twig",parameters: ['resultCombi' => 0,'resultFacto'=> 0,'nValueCombi'=>0,'pValueCombi'=>0,'nValueFact'=>0]);
+        #}
+        #else{
+            $code = $request->query->get('code');
+            $mastermind->test($code);
+            //dd($mastermind);
+            if ($mastermind->isFini()) {
+                return $this->render(view:'/fact/mastermind.html.twig',parameters: ["jeu"=>$mastermind]);
+            }
+            else{
+                return $this->render(view:'/fact/mastermind.html.twig',parameters: ["jeu"=>$mastermind]);
+                
+            }
+        #}
+
+    }
+
 
 
     #[Route('/fact', name: 'app_fact')]
@@ -71,5 +97,56 @@ final class calcul
             return 0;
         }
         return $factN /($factP * $factNmp);//n!/(p!*(n-p)!)
+    }
+}
+
+
+class MastermindService{
+    private $taille;
+    private $number;
+    private $arrayNumber;
+    private $win = false;
+    private $essais = [];
+
+    public function __construct(int $taille){
+        $this->taille = $taille;
+        $temp = 1;
+        for ($i=0; $i < $taille; $i++) { 
+            $temp = $temp * 10;
+        }
+        $this->number =random_int($temp, ($temp*10)-1);
+        $this->arrayNumber = array_map('intval', str_split($this->number));
+    }
+
+    public function test($code){
+
+        $arrayCode  = array_map('intval', str_split($code));
+        
+        if ($code == $this->number) {
+            $this->win = true;
+            array_push($this->essais, [$code, $this->taille]);
+            return true;
+        } else {
+            $nbJuste = 0;
+            for ($i=0; $i < count($arrayCode); $i++) { 
+                if($arrayCode[$i] == $this->arrayNumber[$i]){
+                    $nbJuste++;
+                }
+            }
+            array_push($this->essais, [$code, $nbJuste]);
+            return false;
+        }        
+    }
+
+    public function getEssais(){
+        return $this->essais;
+    }
+
+    public function getTaille(){
+        return $this->taille;
+    }
+
+    public function isFini(){
+        return $this->win;
     }
 }
